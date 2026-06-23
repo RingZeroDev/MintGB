c23e8fa4f789f4a907959ad62475379d05778890
Ring Zero <ringzerodev@proton.me>
2026-06-23 19:41:43 -0400

    Interrupt system stub.

---
+++ src/hardware/include/hardware/interrupt.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+enum class InterruptSource : uint8_t {
+    VBlank = 1 << 0,
+    LCD = 1 << 1,
+    Timer = 1 << 2,
+    Serial = 1 << 3,
+    Joypad = 1 << 4,
+};
+
+class InterruptSystem {
+    private:
+        uint8_t ie; // IE: Interrupt Enable
+        uint8_t if_; // IF: Interrupt Flag
+
+    public:
+        uint8_t readIE();
+        void writeIE(uint8_t value);
+
+        uint8_t readIF();
+        void writeIF(uint8_t value);
+
+        void issueInterrupt(InterruptSource source);
+};
